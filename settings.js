dcd34011f33184d25c1bf814718b3bbfb98e3d25
Sahrull11 <107224311+Sahrull11@users.noreply.github.com>
2022-06-10 07:27:34 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
+  global.ownername = 'ArullOfc' // Ganti Jadi Namamu
+  global.owner = ['6285822347348','6285822347348','6285822347348'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6285822347348' // Owner Utama Istilahnya
+  global.botname = 'ArullBotzMD' // Ganti Jadi Nama Botmu
+  global.packname = 'Sticket By' // Sticker Weem
+  global.author = 'ArullOfc' // Sticker Weem
   global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.linkgroup = 'https://chat.whatsapp.com/LDN0EeJfASs0lbUhJsOJu1' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
   global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.github = 'https://github.com/Arullwara' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/sahrulwara_____' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -56,14 +56,14 @@
 
   // Edit Sesuai Kemauanmu
   global.mess = {
-  admin: '_Lu Admin? 🤨_',
-  botAdmin: '_Bot Bukan Admin 😔_',
-  owner: '_Lu Bukan Owner Gw 😠_',
-  group: '_Khusus Group 😏_',
-  private: '_Khusus Private Chat_',
-  bot: '_Khusus Pengguna Nomer Bot_',
-  wait: '_Sabar Sedang Di Proses ..._',
-  done: '_Done Ngab 🤗_',
+  admin: '*Lu Admin Ngab?* 🤨',
+  botAdmin: '*Bot Bukan Admin 😔*',
+  owner: '*Lu Bukan Owner Gw 😠*',
+  group: '*Khusus Group 😏*',
+  private: '*Khusus Private Chat*',
+  bot: '*Khusus Pengguna Nomer Bot*',
+  wait: '*Sabar Sedang Di Proses ...*',
+  done: '*Done Ngab 🤗*',
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
